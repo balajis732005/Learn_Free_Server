3f0caa9618875bffd81a6a86f1cb130004d43c4f
balajis732005 <balajis07032005@gmail.com>
2025-02-02 21:17:12 +0530

    Initial_Commit

---
+++ src/main/java/com/LearnFree/LearnFreeServer/LearnFreeServerApplication.java
@@ -0,0 +1,13 @@
+package com.LearnFree.LearnFreeServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnFreeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnFreeServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/LearnFree/LearnFreeServer/LearnFreeServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LearnFree.LearnFreeServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnFreeServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
